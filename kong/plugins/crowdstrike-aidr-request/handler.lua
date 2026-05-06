3b77fcc5f967fca189f419ff589dcaa32ac52e65
Kenan Yildirim <kenan.yildirim@crowdstrike.com>
2026-05-06 10:16:29 -0400

    chore: release v0.3.1

--- kong/plugins/crowdstrike-aidr-request/handler.lua
+++ kong/plugins/crowdstrike-aidr-request/handler.lua
@@ -5,7 +5,7 @@ local kong_utils = require("kong.tools.gzip")
 local CrowdStrikeAIDRRequestHandler = {
 	-- Need to run BEFORE ai-proxy
 	PRIORITY = 780,
-	VERSION = "0.3.0",
+	VERSION = "0.3.1",
 }
 
 -- Cap for reading request bodies that nginx has spooled to a temp file.
